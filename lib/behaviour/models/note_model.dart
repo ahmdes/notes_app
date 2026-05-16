53898028eecd8d6588aa2962b5d4607db118a744
ahmed_mostafa <ahmed.mostafa.elshekh@gmail.com>
2026-05-16 14:40:06 +0300

    working on adding hive

--- lib/behaviour/models/note_model.dart
+++ lib/behaviour/models/note_model.dart
@@ -1,9 +1,10 @@
-class NoteModel {
-  String title, content;
-  // int index;
-  NoteModel({
-    required this.content,
-    // required this.index,
-    required this.title,
-  });
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  String title;
+  @HiveField(1)
+  String content;
+  NoteModel({required this.content, required this.title});
 }
